0ee34e45aca01536c6ddcff1d6e82194c215b743
Flaviohmm <fhenrique609@gmail.com>
2020-12-01 20:04:23 -0300

    Componente Mega-Sena #01

--- src/components/mega/Mega.js
+++ src/components/mega/Mega.js
@@ -1,15 +1,29 @@
 import React, { Component } from 'react'
-import { Text, TextInput } from 'react-native'
+import { Button, Text, TextInput } from 'react-native'
 import Estilo from '../estilo'
 
 export default class Mega extends React.Component {
 
     state = {
-        qtdeNumeros: this.props.qtdeNumeros
+        qtdeNumeros: this.props.qtdeNumeros,
+        numeros: []
     }
 
     alterarQtdeNumero = (qtde) => {
-        this.setState({ qtdeNumeros: qtde })
+        this.setState({ qtdeNumeros: +qtde })
+    }
+
+    gerarNumeroNaoContido = nums => {
+        const novo = parseInt(Math.random() * 60) + 1
+        return nums.includes(novo) ? this.gerarNumeroNaoContido(nums) : novo
+    }
+
+    gerarNumeros = () => {
+        const numeros = Array(this.state.qtdeNumeros)
+            .fill()
+            .reduce(nums => [...nums, this.gerarNumeroNaoContido(nums)], [])
+            .sort((a, b) => a - b)
+        this.setState({ numeros })
     }
 
     render() {
@@ -17,15 +31,21 @@ export default class Mega extends React.Component {
             <>
                 <Text style={Estilo.txtG}>
                     Gerador de Mega-Sena
-                {this.state.qtdeNumeros}
                 </Text>
                 <TextInput
                     keyboardType={'numeric'}
                     style={{ borderBottomWidth: 1 }}
                     placeholder="Qtde de Números"
-                    value={this.state.qtdeNumeros}
+                    value={`${this.state.qtdeNumeros}`}
                     onChangeText={this.alterarQtdeNumero}
                 />
+                <Button
+                    title='Gerar'
+                    onPress={this.gerarNumeros}
+                />
+                <Text>
+                    {this.state.numeros.join(',')}
+                </Text>
             </>
         )
     }
